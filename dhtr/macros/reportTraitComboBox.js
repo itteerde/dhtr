8e40e15f42114b6a9cabec9bb5b8c903211abcc7
Erik Itter <erik.itter@gmail.com>
2026-01-19 17:54:18 +0100

    formatting for readability

---
+++ dhtr/macros/reportTraitComboBox.js
@@ -0,0 +1,52 @@
+const keys = new Map([
+    ['agility', 'Agility'],
+    ['finesse', 'Finesse'],
+    ['instinct', 'Instinct'],
+    ['knowledge', 'Knowledge'],
+    ['presence', 'Presence'],
+    ['strength', 'Strength']
+]);
+
+// Build the dropdown HTML
+let dialogContent = `
+    <div style="margin-bottom: 10px;">
+        <label for="trait-select" style="display: block; margin-bottom: 5px;">Select Trait:</label>
+        <select name="choice" id="trait-select" style="width: 100%;">
+`;
+
+for (const [key, label] of keys.entries()) {
+    dialogContent += `<option value="${key}">${label}</option>`;
+}
+
+dialogContent += `</select></div>`;
+
+// https://foundryvtt.com/api/classes/foundry.applications.api.DialogV2.html
+new foundry.applications.api.DialogV2({
+    window: { title: "Traits" },
+    content: dialogContent,
+    buttons: [{
+        action: "choice",
+        label: "Report",
+        default: true,
+        callback: (event, button, dialog) => button.form.elements.choice.value
+    }],
+    submit: result => {
+        if (!keys.has(result)) {
+            console.log("no valid selection");
+        } else {
+            console.log(`User picked option: ${keys.get(result)}`);
+
+            // https://foundryvtt.com/api/classes/foundry.documents.Actor.html
+            let actors = game.folders.getName("The Party").contents.filter(a => a.type === "character");
+
+            let chatMessageContent = `<div style="color: white;"><div style="font-size: large; font-weight: bold;">${keys.get(result)}</div><table style="width: 100%;">`;
+            actors.forEach(a => {
+                chatMessageContent += `<tr style="line-height: 10px;"><td>${a.name}</td><td style="text-align: right; text-align: center;">${a.getRollData().traits[result].value}</td></tr>`;
+            });
+            chatMessageContent += `</table></div>`;
+
+            // https://foundryvtt.com/api/classes/foundry.documents.ChatMessage.html
+            ChatMessage.create({ content: chatMessageContent });
+        }
+    }
+}).render({ force: true });
