f8f507f31383cedebe42b1f8c88a72ad4b3da4b2
Erik Itter <erik.itter@gmail.com>
2026-01-19 13:21:17 +0100

    better traits report (but only traits)

--- dhtr/macros/reportSomething.js
+++ dhtr/macros/reportSomething.js
@@ -2,6 +2,43 @@ let toChat = true;
 let key = 'traits.finesse.value'; // 'traits.finesse.value'; 'experiences'
 let label = 'finesse';
 
+
+const keys = new Map([
+    ['agility', 'Agility'],
+    ['finesse', 'Finesse'],
+    ['instinct', 'Instinct'],
+    ['knowledge', 'Knowledge'],
+    ['presence', 'Presence'],
+    ['strength', 'Strength']
+]);
+
+const content = `
+<form>
+    <div class="form-group">
+        <label for="num">Trait:</label>
+        <div class="form-fields">
+            <select id="trait">${[keys.keys()].reduce((acc, key) => { return acc + `<option value="${key}">${key}</option>` }, "")}</select>
+        </div>
+    </div>
+</form>`;
+
+const buttons = {
+    traits: {
+        icon: "<i class='fa-solid fa-hand-holding-heart'></i>",
+        label: "Traits",
+        callback: async (html) => {
+            const trait = html[0].querySelector("#trait").value;
+            //const skill = TREP2eDB.skills.find(s => s.name === skillName);
+
+            await ChatMessage.create({ content: "<h3>" + trait + "</h3><table>" + TRTacNet.shortenReportSkill(TRTacNet.reportSkill(skillName)).reduce((acc, e) => { return acc += `<tr><td>${e.name}</td><td>${e.roll}</td></tr>`; }, "") + "</table>" + `<div>Aptitude: ${skill.aptitude}</div><div>Types: ${skill.types.reduce((acc, t) => { return acc += t + ", " }, "")}</div>` });
+            return;
+        }
+    }
+}
+
+new Dialog({ title: "Report Traits", content, buttons }).render(true);
+
+
 function getDescendantProp(obj, desc) {
     var arr = desc.split(".");
     while (arr.length && (obj = obj[arr.shift()]));
---
+++ dhtr/macros/reportTrait.js
@@ -0,0 +1,44 @@
+
+const keys = new Map([
+    ['agility', 'Agility'],
+    ['finesse', 'Finesse'],
+    ['instinct', 'Instinct'],
+    ['knowledge', 'Knowledge'],
+    ['presence', 'Presence'],
+    ['strength', 'Strength']
+]);
+
+let dialogContent = '';
+for (const key of keys.keys()) {
+    dialogContent += `
+        <label><input type="radio" name="choice" value="${key}" checked> ${keys.get(key)}</label>
+    `;
+}
+
+new foundry.applications.api.DialogV2({
+    window: { title: "Traits" },
+    content: dialogContent,
+    buttons: [{
+        action: "choice",
+        label: "Report",
+        default: true,
+        callback: (event, button, dialog) => button.form.elements.choice.value
+    }],
+    submit: result => {
+        if (!keys.has(result)) console.log("no valid button");
+        else {
+            console.log(`User picked option: ${keys.get(result)}`);
+
+            let actors = game.folders.getName("The Party").contents.filter(a => a.type === "character");
+            let chatMessageContent = `<div style="color: white;"><div style="font-size: large; font-weight: bold;">${keys.get(result)}</div><table>`;
+
+            actors.forEach(a => {
+                chatMessageContent += `<tr><td>${a.name}</td><td>${a.getRollData().traits[result].value}</td></tr>`;
+            });
+
+            chatMessageContent += `</table></div>`;
+
+            ChatMessage.create({ content: chatMessageContent });
+        }
+    }
+}).render({ force: true });
