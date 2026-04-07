383d55a74d3c2de1f0755daa22158fa0eef42401
Erik Itter <erik.itter@gmail.com>
2026-04-07 21:07:50 +0200

    issue: 11

--- dhtr/macros/tokenGoreHuman.mjs
+++ dhtr/macros/tokenGoreHuman.mjs
@@ -1,14 +1,18 @@
-//token.document.texture.src
-//canvas.tokens.controlled
+const SCOPE = 'dhtr';
+const FLAG_NAME = 'gore';
 
 let tokens = canvas.tokens.controlled
 
 for (const token of tokens) {
-    if (token.document.getFlag("dhtr", "texture.src")) {
-        await token.document.update({ "texture.src": token.document.getFlag("dhtr", "texture.src") });
-        await token.document.setFlag("dhtr", "texture.src", null)
+    if (token.document.getFlag(SCOPE, FLAG_NAME)) {
+        await token.document.update({ "texture.src": token.document.getFlag(SCOPE, FLAG_NAME).texture.src });
+        await token.document.setFlag(SCOPE, FLAG_NAME, null);
     } else {
-        await token.document.setFlag("dhtr", "texture.src", token.document.texture.src);
+        let value = {
+            "texture.src": token.document.texture.src,
+            "texture.scale": token.document.texture.scale
+        };
+        await token.document.setFlag(SCOPE, FLAG_NAME, value);
         await token.document.update({ "texture.src": "Blood_Puddle_w_Body.png" });
     }
 };
